6ae696e580455f501949bacc3fd05bbcc2b257fb
James Fox <jameswfox16@gmail.com>
2019-10-10 17:00:58 -0500

    google maps fix
    fixed issues that were caused by incorrect google map api key

--- cta-data-visual/src/App.js
+++ cta-data-visual/src/App.js
@@ -1,18 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import Button from "@material-ui/core/Button";
-import "./App.css";
-import { AppBar, Toolbar, IconButton } from "@material-ui/core";
-import MainAppBar from "./component/MainAppBar";
+import React from 'react';
+import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import MainAppBar from './component/MainAppBar';
 import GoogleMap from './component/GoogleMap';
 
 function App() {
-  return (
-    <div className="App">
-      <MainAppBar/>
-      <GoogleMap/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MainAppBar />
+        </div>
+    );
 }
 
 export default App;
--- cta-data-visual/src/component/GoogleMap.js
+++ cta-data-visual/src/component/GoogleMap.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import GOOGLE_MAPS_KEY from '../api-keys';
+import { keys } from '@material-ui/core/styles/createBreakpoints';
+
+var googleKey = require('../api-keys.json');
 
 class GoogleMap extends Component {
     static defaultProps = {
         center: {
-            lat: 87.6298,
-            lng: 41.8781
+            lat: 0,
+            lng: -41.81
         },
-        zoom: 11
+        zoom: 2
     };
-    
+
     render() {
-        debugger;
         return (
-            
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: GOOGLE_MAPS_KEY }} //FIX THIS
+                    bootstrapURLKeys={{
+                        key: googleKey.GOOGLE_MAPS_KEY
+                    }} //FIX THIS
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                 ></GoogleMapReact>
