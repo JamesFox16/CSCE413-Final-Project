4dd47340907eeaa3e63b902f0671eb1d73575625
James Fox <jameswfox16@gmail.com>
2019-11-14 17:36:07 -0600

    backend database calls
    made  backend database calls

--- backend/server/db/index.js
+++ backend/server/db/index.js
@@ -12,14 +12,111 @@ const pool = mysql.createPool({
 
 let data = {};
 
-data.all = () => {
+data.boardingTotalAll = () => {
   return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM BoardingTotal`, (err, results) => {
+    pool.query('SELECT * FROM BoardingTotal', (err, results) => {
       if (err) {
         return reject(err);
       }
       return resolve(results);
     });
+  });
+};
+
+data.boardingTotalDate = date => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT * FROM BoardingTotal WHERE service_date = ?',
+      date,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      }
+    );
+  });
+};
+
+data.dailyEntriesAll = () => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM DailyEntriesRail', (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+data.dailyEntriesDate = date => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT * FROM DailyEntriesRail WHERE date = ?',
+      date,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      }
+    );
+  });
+};
+
+data.dailyEntriesStationId = stationId => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT * FROM DailyEntriesRail WHERE station_id = ?',
+      stationId,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      }
+    );
+  });
+};
+
+data.monthlyEntriesRailAll = () => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM MonthlyEntriesRail', (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+data.monthlyEntriesRailMonth = monthBegin => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT * FROM MonthlyEntriesRail WHERE month_beginning = ?',
+      monthBegin,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      }
+    );
+  });
+};
+
+data.monthlyEntriesRailStationId = stationId => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT * FROM MonthlyEntriesRail WHERE station_id = ?',
+      stationId,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      }
+    );
   });
 };
 
--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -5,26 +5,86 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    let results = await db.all();
+    let results = await db.boardingTotalAll();
     res.json(results);
   } catch (e) {
     res.sendStatus(500);
   }
 });
 
-router.get('/boardingtotal', (req, res, next) => {
-  res.json({ test: 'test' });
+router.get('/boardingtotal', async (req, res, next) => {
+  try {
+    let results = await db.boardingTotalAll();
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
-router.get('/derail', (req, res, next) => {
-  res.json({ test: 'test' });
+router.get('/boardingtotal/date/:date', async (req, res, next) => {
+  try {
+    let results = await db.boardingTotalDate(req.params.date);
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
-router.get('/lstop', (req, res, next) => {
-  res.json({ test: 'test' });
+router.get('/derail', async (req, res, next) => {
+  try {
+    let results = await db.dailyEntriesAll();
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
-router.get('/merail', (req, res, next) => {
+router.get('/derail/date/:date', async (req, res, next) => {
+  try {
+    let results = await db.dailyEntriesDate(req.params.date);
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/derail/station/:station', async (req, res, next) => {
+  try {
+    let results = await db.dailyEntriesStationId(req.params.station);
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/merail', async (req, res, next) => {
+  try {
+    let results = await db.monthlyEntriesRailAll();
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/merail/month/:month', async (req, res, next) => {
+  try {
+    let results = await db.monthlyEntriesRailMonth(req.params.month);
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/merail/station/:station', async (req, res, next) => {
+  try {
+    let results = await db.monthlyEntriesRailStationId(req.params.station);
+    res.json(results);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/lstop', async (req, res, next) => {
   res.json({ test: 'test' });
 });
 
